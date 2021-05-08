149561b34f9d73650b45be5a3ae4f134b2e8b95b
jaelse <j.ronald@hydroneo.net>
2021-05-08 14:47:22 +0700

    small changes

--- src/main/java/com/example/aqoda/resource/hotel/entity/HotelEntity.java
+++ src/main/java/com/example/aqoda/resource/hotel/entity/HotelEntity.java
@@ -1,4 +1,24 @@
 package com.example.aqoda.resource.hotel.entity;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
 public class HotelEntity {
+
+    @Id
+    private Long id;
+
+    private  String name;
+
+    private Integer noOfFloors;
+
+    private List<RoomEntity> rooms;
+
 }
